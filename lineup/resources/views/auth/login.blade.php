47964ee09062872c2ecb464b7b54fbecac72af95
Federioc Crameri <federico.crameri@hotmail.it>
2016-10-17 19:14:01 +0200

    login design modified

--- lineup/resources/views/auth/login.blade.php
+++ lineup/resources/views/auth/login.blade.php
@@ -1,11 +1,12 @@
-@extends('layouts.app')
+@extends('layouts.authmain')
 
 @section('content')
 <div class="container">
     <div class="row">
+
         <div class="col-md-8 col-md-offset-2">
+            <h1>Login</h1>
             <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
