2b1860d912f804344c76980969e9889cb44bd8a2
Federioc Crameri <federico.crameri@hotmail.it>
2016-10-17 20:25:26 +0200

    email validation for NTB user added

--- lineup/resources/views/layouts/authmain.blade.php
+++ lineup/resources/views/layouts/authmain.blade.php
@@ -29,6 +29,7 @@
     <div class = "collapse navbar-collapse" id = "example-navbar-collapse">
         <ul class = "nav navbar-nav navbar-right">
             <li><a href="{{ url('/register') }}">Register</a></li>
+            <li><a href="{{ url('/login') }}">Login</a></li>
         </ul>
    </div>
 </nav>
