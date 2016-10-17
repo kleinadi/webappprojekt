ceaea102d05d4eb664ee3b3ce585b9c7763e6d64
Geo Bontognali <geo.bontognali@ntb.ch>
2016-10-18 00:46:05 +0200

    digital and analog clock added

--- lineup/resources/views/settings.blade.php
+++ lineup/resources/views/settings.blade.php
@@ -14,11 +14,13 @@
 
             <div class="panel panel-default">
                 <div class="panel-heading">
-                    Add Module
+                    Select Modules
                 </div>
 
                 <div class="panel-body">
-
+                    Find and add the modules you want to show on your schedule.<br/>
+                    If not present feel free to create a new one.<br />
+                    <br />
                     <div id="custom-search-input">
                         <div class="input-group col-md-12">
                             <input type="text" class="form-control " id="module-search" placeholder="Search Modules" />
@@ -36,14 +38,30 @@
                     </div>
 
                 </div>
+                <script language="javascript">
+                    var moduleFinder = new ModuleFinder();
+                </script>
+            </div>
 
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    Add new Module
+                </div>
+
+                <div class="panel-body">
+                    Create a new Module to be added to the list.<br />
+                    Please make sure that your information is correct.<br />
+                    <br />
+
+
+                </div>
 
             </div>
+
+
         </div>
     </div>
 
-    <script language="javascript">
-        var moduleFinder = new ModuleFinder();
-    </script>
+
 
 @endsection
