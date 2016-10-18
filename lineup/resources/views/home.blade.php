044ae63aee0dcaa684cfecaa400fbec04f31b9fe
Federioc Crameri <federico.crameri@hotmail.it>
2016-10-19 00:15:31 +0200

    schedule designs

--- lineup/resources/views/home.blade.php
+++ lineup/resources/views/home.blade.php
@@ -1,10 +1,38 @@
 @extends('layouts.main')
 
 @section('content')
-    <div class="row">
 
-                <div class="panel-body">
-                    You are logged in!
+        <!-- Stack the columns on mobile by making one full-width and the other half-width -->
+        <div class="row">
+            <div class="col-xs-5 col-md-2" style="min-width: 150px">
+                <div class="panel panel-default">
+                        <div class="panel-body">08:15 - 10:10</div>
                 </div>
-    </div>
+            </div>
+            <div class="col-xs-7 col-md-10" id="columnscroll">
+                <div class="row">
+                    <div class="col-md-2 rawscroll">
+                        <div class="panel panel-default"><div class="panel-body panelboarder">schedule</div></div>
+                    </div>
+                    <div class="col-md-2 rawscroll">
+                        <div class="panel panel-default"><div class="panel-body panelboarder">schedule</div></div>
+                    </div>
+                    <div class="col-md-2 rawscroll">
+                        <div class="panel panel-default"><div class="panel-body panelboarder">schedule</div></div>
+                    </div>
+                    <div class="col-md-2 rawscroll">
+                        <div class="panel panel-default"><div class="panel-body panelboarder">schedule</div></div>
+                    </div>
+                    <div class="col-md-2 rawscroll">
+                        <div class="panel panel-default"><div class="panel-body panelboarder">schedule</div></div>
+                    </div>
+                    <div class="col-md-2 rawscroll">
+                        <div class="panel panel-default"><div class="panel-body panelboarder">schedule</div></div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+
 @endsection
