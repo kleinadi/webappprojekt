d31bb6fb49f1b0665b4dedb601f4040c309be477
Geo Bontognali <geo.bontognali@ntb.ch>
2016-10-18 19:42:30 +0200

    settings page development

--- lineup/resources/views/home.blade.php
+++ lineup/resources/views/home.blade.php
@@ -1,10 +1,37 @@
 @extends('layouts.main')
 
 @section('content')
+    <!-- Modal -->
+    <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog modal-lg">
+
+            <!-- Modal content-->
+            <div class="modal-content">
+                <div class="modal-header" style="text-align: center;">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h1 class="modal-title">Empty Popup</h1>
+                </div>
+                <div class="modal-body">
+
+
+
+
+
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Cancel</button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
     <div class="row">
 
                 <div class="panel-body">
                     You are logged in!
+                    <!-- Trigger the modal with a button -->
+                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Open Modal</button>
                 </div>
     </div>
 @endsection
--- lineup/resources/views/settings.blade.php
+++ lineup/resources/views/settings.blade.php
@@ -49,9 +49,54 @@
                 </div>
 
                 <div class="panel-body">
-                    Create a new Module to be added to the list.<br />
+                    Create a new Module to be added. Your entry will be available to everyone.<br />
                     Please make sure that your information is correct.<br />
                     <br />
+
+                    <form>
+                        <div class="form-group">
+                            <label for="modulName">Name:</label>
+                            <input type="email" class="form-control" id="email" placeholder="i.e. MAS_A">
+                        </div>
+                        <div class="form-group">
+                            <label for="fullName">Full Name:</label>
+                            <input type="password" class="form-control" id="pwd" placeholder="i.e. Mehrdimensionale Analysis">
+                        </div>
+                        <div class="form-group">
+                            <label for="professor">Professor:</label>
+                            <input type="password" class="form-control" id="pwd" placeholder="i.e. Hugo Reyes">
+                        </div>
+                        <div class="form-group">
+                            <label for="room">Room:</label>
+
+                            <input type="password" class="form-control" id="pwd" placeholder="i.e. A1.3">
+
+                        </div>
+
+                        <div class="form-group">
+                            <div class="form-group">
+                                <label for="sel1">Times:</label>
+                                <select class="form-control" id="sel1">
+                                    <option>Select Day</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                </select>
+
+                                <select class="form-control" id="sel1">
+                                    <option>Select Time</option>
+                                    <option>2</option>
+                                    <option>3</option>
+                                    <option>4</option>
+                                </select>
+                            </div>
+                        </div>
+
+
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Add Module</button>
+                        </div>
+                    </form>
 
 
                 </div>
