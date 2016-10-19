eae66ec8cb01a3221078f822c59f75188e5f0570
Federioc Crameri <federico.crameri@hotmail.it>
2016-10-19 15:55:22 +0200

    timetable design improvements

--- lineup/resources/views/home.blade.php
+++ lineup/resources/views/home.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 
-    <div class="col-xs-5 col-md-2 panelspacing" id="timeraw"></div>
-    <div class="col-xs-7 col-md-10" id="columnscroll"></div>
+        <div class="col-xs-5 col-sm-2 col-md-2  col-lg-2 panelspacing" id="timeraw"></div>
+        <div class="col-xs-7 col-sm-10 col-md-10 col-lg-10" id="columnscroll"></div>
 
 
     <script language="javascript">
