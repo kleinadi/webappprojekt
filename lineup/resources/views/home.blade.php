7ae604dfaaa6aa6ed10e0f3d9371b55a65e3a7ea
Geo Bontognali <geo.bontognali@ntb.ch>
2016-10-20 01:14:49 +0200

    better structure for the schedule on mobile

--- lineup/resources/views/home.blade.php
+++ lineup/resources/views/home.blade.php
@@ -2,11 +2,163 @@
 
 @section('content')
 
-        <div class="col-xs-5 col-sm-2 col-md-2  col-lg-2 panelspacing" id="timeraw"></div>
-        <div class="col-xs-7 col-sm-10 col-md-10 col-lg-10" id="columnscroll"></div>
+        <!--  -->
+
+        <!-- Desktop HTML -->
+        <div class="container-fluid hidden-sm hidden-xs hidden-md" >
+            <div class="col-lg-1" style="max-width: 145px;">
+                <h2>Time</h2>
+                <div class="panel panel-default rawspacing invisiblepanel"><div class="panel-body">08:15<br /> 09:50</div></div>
+                <div class="panel panel-default rawspacing invisiblepanel"><div class="panel-body">10:10<br /> 11:45</div></div>
+                <div id="breakbar"></div>
+                <div class="panel panel-default rawspacing invisiblepanel"><div class="panel-body">13:15<br /> 14:50</div></div>
+                <div class="panel panel-default rawspacing invisiblepanel"><div class="panel-body">15:15<br /> 16:50</div></div>
+                <div class="panel panel-default rawspacing invisiblepanel"><div class="panel-body">17:00<br /> 19:45</div></div>
+            </div>
+            <div class="col-lg-11">
+                <div class="row">
+                    <div class="col-lg-2 rawscroll panelspacing"><h2>Mon</h2><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-lg-2 rawscroll panelspacing"><h2>Tue</h2><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-lg-2 rawscroll panelspacing"><h2>Wed</h2><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-lg-2 rawscroll panelspacing"><h2>Thu</h2><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-lg-2 rawscroll panelspacing"><h2>Fri</h2><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-lg-2 rawscroll panelspacing"><h2 style="color: #72b1ff;" >Sat</h2><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing "><div class="panel-body modulepane">schedule</div></div></div>
+                </div>
+
+                <div id="breakbar"></div>
+
+                <div class="row">
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing "><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing "><div class="panel-body modulepane">schedule</div></div></div>
+                    <div class="col-md-2 rawscroll panelspacing"><div class="panel panel-default rawspacing bluepanel"><div class="panel-body modulepane">schedule</div></div></div>
+                </div>
+            </div>
+        </div>
 
 
-    <script language="javascript">
-        var createTable = new CreateTable();
-    </script>
+        <!-- Mobile HTML -->
+        <div class="hidden-lg">
+            <div class="container monday">
+                <h2>Mon</h2>
+                <table class="scheduleTable">
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                </table>
+            </div>
+
+
+            <div class="container monday">
+                <h2>Tue</h2>
+                <table class="scheduleTable">
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="container monday">
+                <h2>Wed</h2>
+                <table class="scheduleTable">
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="container monday">
+                <h2>Tue</h2>
+                <table class="scheduleTable">
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                    <tr>
+                        <td class="timeCol">08:15 09:50</td>
+                        <td><div class="panel panel-default rawspacing"><div class="panel-body modulepane">schedule</div></div></td>
+                    </tr>
+                </table>
+            </div>
+
+        </div>
+
+
+
+
+
 @endsection
--- lineup/resources/views/layouts/main.blade.php
+++ lineup/resources/views/layouts/main.blade.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="css/clock.css">
     <script src="js/code.js"></script>
     <script src="js/clock.js"></script>
-    <script src="js/schedule.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
