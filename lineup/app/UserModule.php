738ddd9faed03985a2092f498c72c818b670fec0
Federioc Crameri <federico.crameri@hotmail.it>
2016-10-20 01:50:00 +0200

    minor bug fix with design

---
+++ lineup/app/UserModule.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: feder
+ * Date: 20.10.2016
+ * Time: 01:40
+ */
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class UserModule extends Model
+{
+    // Table Name
+    protected $table = 'usermodule';
+}
